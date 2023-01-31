f23c060975d226f819ff1343eff9d06146356d4a
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-31 10:18:27 -0300

    basic background

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactFlow, { Background, Controls } from 'reactflow';
+import 'reactflow/dist/style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: '100vw', height: '100vh'}}>
+      <ReactFlow>
+        <Background
+          gap={12}
+          size={2}
+          color='#ddd'
+        />
+      </ReactFlow>
     </div>
   );
 }
